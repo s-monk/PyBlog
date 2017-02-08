6f6ded1b85f7fd8550f024a60ddb46fafd6224cc
s.monk <bluemonk78@gmail.com>
2017-02-08 11:13:27 -0500

    added db , added templates and url routing framework, set routeing from urls to views, added search

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from app import app #import our Flask app
+from app import app, db #import our Flask app
+import models
 import views
+
+#registering blueprint
+from entries.blueprint import entries 
+app.register_blueprint(entries, url_prefix='/entries')
 
 if __name__ == '__main__':
     app.run()
