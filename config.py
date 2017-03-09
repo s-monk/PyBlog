b00c0782e2b76579420230cdfa66d321ab522498
s.monk <bluemonk78@gmail.com>
2017-03-09 14:20:51 -0500

    added the create & edit & image upload capabilities

--- config.py
+++ config.py
@@ -3,5 +3,8 @@ import os
 class Configuration(object):
     APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
     DEBUG = True
+    SECRET_KEY = 'flask is fun!' #create a unique key for ap
     SQLALCHEMY_DATABASE_URI = 'sqlite:///%s/blog.db' % APPLICATION_DIR
     SQLALCHEMY_TRACK_MODIFICATIONS = True
+    STATIC_DIR = os.path.join(APPLICATION_DIR, 'static')
+    IMAGES_DIR = os.path.join(STATIC_DIR, 'images')
--- entries/blueprint.py
+++ entries/blueprint.py
@@ -1,28 +1,58 @@
-from flask import Blueprint
-from flask import render_template
-from flask import request
+from flask import Blueprint, flash, redirect, render_template, g, request, url_for
+from werkzeug import secure_filename
 
 from helpers import object_list
 from models import Entry, Tag
+from app import app, db
+from entries.forms import EntryForm, ImageForm
+import os
 
 entries = Blueprint('entries', __name__, template_folder='templates')
 
 def entry_list(template, query, **context):
-    search = request.args.get('q')
-    if search:
+    valid_statuses = (Entry.STATUS_PUBLIC, Entry.STATUS_DRAFT)
+    query = query.filter(Entry.status.in_(valid_statuses))
+    if request.args.get('q'):
+        search = request.args.get['q']
         query = query.filter(
             (Entry.body.contains(search)) |
             (Entry.title.contains(search)))
+
     return object_list(template, query, **context)
+
+def get_entry_or_404(slug, author=None):
+    query = Entry.query.filter(Entry.slug == slug)
+   # if author:
+       # query = query.filter(Entry.author == author)
+    #else:
+       # query = filter_status_by_user(query)
+    return query.first_or_404()
 
 @entries.route('/')
 def index():
     entries = Entry.query.order_by(Entry.created_timestamp.desc())
     return entry_list('entries/index.html', entries)
 
+@entries.route('/image-upload/', methods=['GET', 'POST'])
+def image_upload():
+    if request.method == 'POST':
+        form = ImageForm(request.form)
+        if form.validate():
+            image_file = request.files['file']
+            filename = os.path.join(app.config['IMAGES_DIR'], 
+                secure_filename(image_file.filename))
+            image_file.save(filename)
+            flash('Saved %s' % os.path.basename(filename), 'success')
+            return redirect(url_for('entries.index'))
+    else:
+        form = ImageForm()
+
+    return render_template('entries/image_upload.html', form=form)
+
+
 @entries.route('/tags/')
 def tag_index():
-    tags = Tag.query.filter(Tag.name)
+    tags = Tag.query.order_by(Tag.name)
     return object_list('entries/tag_index.html', tags)
 
 @entries.route('/tags/<slug>/')
@@ -31,8 +61,51 @@ def tag_detail(slug):
     entries = tag.entries.order_by(Entry.created_timestamp.desc())
     return entry_list('entries/tag_detail.html', entries, tag=tag)
 
+@entries.route('/create/', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        form = EntryForm(request.form)
+        if form.validate():
+            entry = form.save_entry(Entry())
+            db.session.add(entry)
+            db.session.commit()
+            flash('Entry "%s" created successfully' % entry.title, 'success')
+            return redirect(url_for('entries.detail', slug=entry.slug))
+    else:
+        form = EntryForm()
+    
+    return render_template('entries/create.html', form=form)
+
+@entries.route('/<slug>/edit/', methods=['GET', 'POST'])
+def edit(slug):
+    entry = get_entry_or_404(slug)
+    if request.method == 'POST':
+        form = EntryForm(request.form, obj=entry)
+        if form.validate():
+            entry = form.save_entry(entry)
+            db.session.add(entry)
+            db.session.commit()
+            flash('Entry "%s" has ben saved' % entry.title, 'success')
+            return redirect(url_for('entries.detail', slug=entry.slug))
+    else:
+        form = EntryForm(obj=entry)
+
+    return render_template('entries/edit.html', entry=entry, form=form)
+
+@entries.route('/<slug>/delete/', methods=['GET', 'POST'])
+def delete(slug):
+    entry = get_entry_or_404(slug)
+    if request.method == 'POST':
+        entry.status = Entry.STATUS_DELETED
+        db.session.add(entry)
+        db.session.commit()
+        flash('Entry "%s" has been deleted' % entry.title, 'success')
+        return redirect(url_for('entries.index'))
+
+    return render_template('entries/delete.html', entry=entry)
+
 @entries.route('/<slug>/')
 def detail(slug):
-    entry = Entry.query.filter(Entry.slug == slug).first_or_404()
+    entry = get_entry_or_404(slug)
     return render_template('entries/detail.html', entry=entry)
 
